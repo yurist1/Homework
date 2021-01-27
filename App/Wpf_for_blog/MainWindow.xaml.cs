1054b4294e2c5a91d0cc577a8d745dffa47315dd
yrchoi <yrchoi@trenbe.com>
2021-01-27 18:46:10 +0900

    Blog, 의존 프로퍼티 샘플

--- App/Wpf_for_blog/MainWindow.xaml.cs
+++ App/Wpf_for_blog/MainWindow.xaml.cs
@@ -20,9 +20,55 @@ namespace Wpf_for_blog
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int count;
         public MainWindow()
         {
+            count = 0;
             InitializeComponent();
+        }
+
+        //의존 속성 생성 및 등록!!!! 완전 중요 !!!!!! 
+        public static readonly DependencyProperty BgColorProperty =
+                               DependencyProperty.Register("BgColor", typeof(string), typeof(MainWindow), new FrameworkPropertyMetadata(new PropertyChangedCallback(OnBgColorPropertyChange)));
+
+        public string BgColor 
+        {
+            get 
+            {
+                return GetValue(BgColorProperty).ToString();
+            }
+            set 
+            {
+                SetValue(BgColorProperty, value);
+            }
+        }
+
+        //속성이 변경될 때마다 실행 됨!!!! 이것도 완전 중요!!!!!! 
+        public static void OnBgColorPropertyChange(DependencyObject d, DependencyPropertyChangedEventArgs e) 
+        {
+            MainWindow window = d as MainWindow;
+            SolidColorBrush brush = (SolidColorBrush)new BrushConverter().ConvertFromString(e.NewValue.ToString());
+            window.btn.Background = brush;
+        }
+
+        // 변수처럼 사용된 애가 이거임. 
+        //버튼이 클릭할 때 버튼 색이 바뀌도록 구현 
+        //프로퍼티에 어떤 변화를 줄것인지 구현하면 됌
+        private void BtnClick(object sender, RoutedEventArgs e) 
+        {
+            count++;
+            if (count % 3 == 0)
+            {
+                BgColor = "Red";
+            } 
+            else if (count % 3 == 1) 
+            {
+                BgColor = "Green";
+            }
+            else 
+            {
+                BgColor = "Blue";
+            }
         }
     }
 }
