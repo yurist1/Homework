016c4c6f8d4012f04e44003e3d406332bf73d256
yrchoi <yrchoi@trenbe.com>
2021-05-11 18:52:44 +0900

    알고리즘, 단어 퀴즈 진행 중

--- App/알고리즘/Tournament/Program.cs
+++ App/알고리즘/Tournament/Program.cs
@@ -10,44 +10,13 @@ namespace Tournament
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("정답 => " + solution(8, 2, 3));
+            var tournament = new Tournament().solution(8, 2, 3);
+            var wordPuzzle = new WordPuzzle().solution(new string[] { "app", "ap", "p", "l", "e", "ple", "pp" }, "apple");
+
+            Console.WriteLine("정답 => "+wordPuzzle);
             Console.ReadLine();
         }
 
-        public static int solution(int n, int a, int b)
-        {
-            int answer = DoGame(0,n, a, b);
-
-
-
-            return answer;
-        }
-        public static int DoGame(int gameInd, int players, int A, int B) 
-        {
-            if (players == 2 || (Math.Abs(A-B) == 1 && Math.Max(A,B)%2 ==0))
-            {
-                return ++gameInd;
-            }
-            else 
-            {
-                var result = new int[players / 2];
-                int ind = 1;
-                int A1 = 0;
-                int B1 = 0;
-                while (ind<= players) 
-                {
-                    if (result[(ind-1) / 2] == 0 || ind == A || ind ==B )  
-                    {
-                        result[(ind - 1) / 2] = ind;
-                        if (ind == A)
-                            A1 = ((ind - 1) / 2)+1;
-                        else if (ind == B)
-                            B1 = ((ind - 1) / 2) + 1;
-                    }
-                    ind++;
-                }
-                return DoGame(++gameInd, result.Length, A1, B1);
-            }
-        }
+        
     }
 }
---
+++ App/알고리즘/Tournament/WordPuzzle.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Tournament
+{
+    public class WordPuzzle
+    {
+        private string[] puzzles;
+        private List<int> indexs;
+        public WordPuzzle() 
+        {
+        
+        }
+        public int solution(string[] strs, string t)
+        {
+            puzzles = strs;
+            int ind = 0;
+
+            indexs = new List<int>();
+            var test = MatchPuzzle(t, ind);
+             return indexs.Count() == 0 ? -1: indexs.Min();
+        }
+
+        public int MatchPuzzle(string target, int ind) 
+        {
+                bool isMatched = false;
+                foreach (var puzzle in puzzles) 
+                {
+                    if (Regex.IsMatch(target, "[a-zA-Z]"))
+                    {
+                        if (target.Contains(puzzle))
+                        {
+                            var regex = new Regex(Regex.Escape(puzzle));
+
+                            isMatched = MatchPuzzle(regex.Replace(target, " ", 1), ind+1) == -1 ? false: true;
+                        }
+                    }
+                    else 
+                    {
+                        indexs.Add(ind);
+                        break;
+                    }
+                       
+                }
+
+                if (!isMatched)
+                    return -1;
+            
+
+            return ind;
+        }
+        
+    }
+}
