11356bb01ab8e074ba3b859f155e7bb1ff5e39a5
yrchoi <yrchoi@trenbe.com>
2021-06-01 18:57:57 +0900

    알고리즘, 124 나라

---
+++ App/알고리즘/Tournament/ConvertNum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament
+{
+    class ConvertNum
+    {
+        public string solution(int n)
+        {
+
+            return convertInto124(n);
+        }
+        private string convertInto124(int num)
+        {
+            int target = num;
+            string list = "";
+            while (true)
+            {
+                if (target <= 3)
+                {
+                    list = target + list;
+                    break;
+                }
+
+                if (target % 3 == 0)
+                {
+                    list = 3 + list;
+                    target = (target / 3) - 1;
+                }
+                else
+                {
+                    list = (target % 3) + list;
+                    target = target / 3;
+                }
+            }
+
+            list.Replace("3", "4");
+
+
+            return list.Replace("3", "4");
+        }
+    }
+}
--- App/알고리즘/Tournament/Program.cs
+++ App/알고리즘/Tournament/Program.cs
@@ -11,9 +11,10 @@ namespace Tournament
         static void Main(string[] args)
         {
             var tournament = new Tournament().solution(8, 2, 3);
-            var wordPuzzle = new WordPuzzle().solution(new string[] { "app", "ap", "p", "l", "e", "ple", "pp" }, "apple");
+            //var wordPuzzle = new WordPuzzle().solution(new string[] { "app", "ap", "p", "l", "e", "ple", "pp" }, "apple");
+            var convertnum = new ConvertNum().solution(300);
 
-            Console.WriteLine("정답 => "+wordPuzzle);
+            Console.WriteLine("정답 => "+ convertnum);
             Console.ReadLine();
         }
 
